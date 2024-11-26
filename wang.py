17ab6f924717930000f1493a5ce3241194e2c907
vinicius <vinicius.lima.cordeiro@gmail.com>
2024-11-26 18:49:15 +0100

    up

--- wang.py
+++ wang.py
@@ -34,7 +34,7 @@ D = (D * fsamp).astype(int)
 
 f = 40  # np.linspace(20, 60, Nareas)[::-1]  # Node natural frequency in Hz
 
-muee = 1
+muee = 10
 flnMat = (1 + eta * h[:, None]) * flnMat
 
 Iext = np.zeros((Nareas, Npoints))
@@ -184,113 +184,26 @@ def _mi(S, roi_x, roi_y, stim):
     return xr.DataArray(mi, dims=("times", "freqs"), coords=(times, freqs))
 
 
-# Define the function to compute MI using HOI and JAX
-mi_fcn = get_mi("gc")
+pairs = np.stack([[[0] * 28], [range(1, 29)]], axis=1).squeeze().T
 
-# vectorize the function to first and second dimension
-gcmi = jax.vmap(jax.vmap(mi_fcn, in_axes=0), in_axes=0)
-
-# Select data for nodes
-x = S.sel(roi=["V1"]).data.squeeze()
-y = S.sel(roi=["V4"]).data.squeeze()
-z = S.sel(roi=["24c"]).data.squeeze()
-
-# Edge activity (with and without normalisation)
-e1 = x * np.conj(y)
-e2 = y * np.conj(z)
-e3 = x * np.conj(z)
-
-# Real and Imag parts pf edge activity
-e1r, e1i = np.real(e1), np.imag(e1)
-e2r, e2i = np.real(e2), np.imag(e2)
-e3r, e3i = np.real(e3), np.imag(e3)
-
-# Stack complex values
-E1 = np.stack((e1r, e1i), axis=1)
-E2 = np.stack((e2r, e2i), axis=1)
-E3 = np.stack((e3r, e3i), axis=1)
-E12 = np.stack((e1r, e1i, e2r, e2i), axis=1)
-E23 = np.stack((e2r, e2i, e3r, e3i), axis=1)
-E123 = np.stack((e1r, e1i, e2r, e2i, e3r, e3i), axis=1)
-
-# Swap axis 0 with 3 and 1 with 2
-E1 = np.moveaxis(E1, [0, 1], [-1, -2])
-E2 = np.moveaxis(E2, [0, 1], [-1, -2])
-E3 = np.moveaxis(E3, [0, 1], [-1, -2])
-E12 = np.moveaxis(E12, [0, 1], [-1, -2])
-E23 = np.moveaxis(E23, [0, 1], [-1, -2])
-E123 = np.moveaxis(E123, [0, 1], [-1, -2])
-
-# Stims across trials
+rois = S.roi.values
 stim = data.trials.values
-stim = np.expand_dims(stim, axis=(0, 1))
-stim = np.tile(stim, (len(freqs), data.sizes["times"], 1, 1))
 
-# Copnorm
-E1 = copnorm_nd(E1, axis=-1)
-E2 = copnorm_nd(E2, axis=-1)
-E3 = copnorm_nd(E3, axis=-1)
-E12 = copnorm_nd(E12, axis=-1)
-E23 = copnorm_nd(E23, axis=-1)
-E123 = copnorm_nd(E123, axis=-1)
-stim = copnorm_nd(stim, axis=-1)
+out = []
+for i, j in tqdm(pairs):
+    out += [_mi(S, rois[i], rois[j], stim)]
 
-# MIF for edge encoding
-mif_e1 = gcmi(E1, stim).T
-mif_e2 = gcmi(E2, stim).T
-mif_e3 = gcmi(E3, stim).T
-mif_e12 = gcmi(E12, stim).T
-mif_e23 = gcmi(E23, stim).T
-mif_e123 = gcmi(E123, stim).T
 
-red = np.minimum(mif_e1, mif_e2)
+plt.figure(figsize=(40, 40))
 
-syn = mif_e123 - np.maximum(mif_e1, mif_e2)
-
-## Plot
-
-times = data.times.values
-ax = plt.subplot()
-# Plot single-trial and time-frequency MI
-mi = xr.DataArray(mif_e3, dims=("times", "freqs"), coords=(times, freqs))
-# Plot trial-average time-frequency MI
-mi.plot(x="times", y="freqs", cmap="turbo", vmin=0)
-# mi.plot.title('ciao')
-ax.set_title(
-    "Coherence between node 2 and 3 using Frequency-domain MI (Kuramoto Chain)"
-)
-ax.set_xlabel("Time (samples)")
-ax.set_ylabel("Frequency (Hz)")
-plt.show()
-
-ax = plt.subplot()
-# Plot single-trial and time-frequency MI
-mi = xr.DataArray(mif_e123, dims=("times", "freqs"), coords=(times, freqs))
-# Plot trial-average time-frequency MI
-mi.plot(x="times", y="freqs", cmap="turbo", vmin=0)
-# mi.plot.title('ciao')
-ax.set_title("Total MI I(e1, e2; coupling)")
-ax.set_xlabel("Time (samples)")
-ax.set_ylabel("Frequency (Hz)")
-plt.show()
-
-ax = plt.subplot()
-# Plot single-trial and time-frequency MI
-mi = xr.DataArray(red, dims=("times", "freqs"), coords=(times, freqs))
-# Plot trial-average time-frequency MI
-mi.plot(x="times", y="freqs", cmap="turbo", vmin=0)
-# mi.plot.title('ciao')
-ax.set_title("Redundancy")
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Frequency (Hz)")
-plt.show()
-
-ax = plt.subplot()
-# Plot single-trial and time-frequency MI
-mi = xr.DataArray(syn, dims=("times", "freqs"), coords=(times, freqs))
-# Plot trial-average time-frequency MI
-mi.plot(x="times", y="freqs", cmap="turbo", vmin=0)
-# mi.plot.title('ciao')
-ax.set_title("Synergy")
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Frequency (Hz)")
+for pos, _out in enumerate(out):
+    plt.subplot(6, 5, pos + 1)
+    i, j = pairs[pos]
+    _out.T.plot(cmap="turbo", vmin=0, vmax=1)
+    plt.title(f"{rois[i]}-{rois[j]}")
+    plt.xticks(fontsize=9)
+    plt.yticks(fontsize=9)
+    plt.ylabel("freq [Hz]", fontsize=9)
+    plt.xlabel("time [s]", fontsize=9)
+plt.tight_layout()
+plt.savefig("mi_cond.png")
