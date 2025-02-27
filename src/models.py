dc42d66da48d7a325e6f2971dcdbc9b596af078c
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2025-02-27 16:11:06 +0100

    up

--- src/models.py
+++ src/models.py
@@ -17,6 +17,14 @@ def _ode(Z: np.complex128, a: float, w: float):
     return Z * (a + 1j * w - jnp.abs(Z * Z))
 
 
+def drdt(r, a, g, S, Isyn, Iext):
+    return (a - g * S - r * r) * r + g * Isyn + Iext
+
+
+def d0dt(omega, g, Isyn, Iext):
+    return omega + g * Isyn + Iext
+
+
 def simulate(
     A: np.ndarray,
     g: float,
@@ -76,7 +84,9 @@ def simulate(
         exp_phi = gain * jnp.exp(1j * (jnp.angle(phases_t) + phi)) + offset
 
         # Input to each node
-        Input = (A * phase_differences).sum(axis=1) + Iext[:, t] * exp_phi
+        Input = (A * phase_differences).sum(axis=1) + Iext[:, t] * exp_phi * jnp.sqrt(
+            jnp.abs(phases_t)
+        )
 
         phases_history = phases_history.at[:, 0].set(
             phases_t
@@ -92,6 +102,74 @@ def simulate(
     _, phases = jax.lax.scan(_loop, (phases_history), times)
 
     return phases[::decim]
+
+
+def simulate2(
+    A: np.ndarray,
+    g: float,
+    f: float,
+    a: float,
+    fs: float,
+    eta: float,
+    T: float,
+    Iext: np.ndarray = None,
+    alpha: float = 1,
+    seed: int = 0,
+    device: str = "cpu",
+    decim: int = 1,
+):
+
+    assert device in ["cpu", "gpu"]
+
+    jax.config.update("jax_platform_name", device)
+
+    N, A, omegas, phases_history, dt, a = _set_nodes(A, f, fs, a)
+
+    if Iext is not None:
+        Iext = jnp.asarray(Iext)  # Assure it is a jax ndarray
+
+    r = jnp.abs(phases_history).astype(jnp.float32)
+    theta = jnp.angle(phases_history).astype(jnp.float32)
+
+    times = np.arange(T, dtype=int)  # Time array
+
+    # Scale witgh dt to avoid doing it evert time-step
+    # A = g * A * dt
+    # Nodes strength
+    S = A.sum(axis=1)
+    eta = eta * jnp.sqrt(dt)
+    Iext_r = Iext * alpha
+    Iext_0 = Iext * (1 - alpha)
+
+    @jax.jit
+    def _loop(carry, t):
+
+        r, theta = carry
+        r_t, theta_t = r.squeeze().copy(), theta.squeeze().copy()
+
+        phase_differences = -(theta - theta_t)
+        amp_ratios = r / r_t
+
+        Isyn_r = (A * jnp.tile(r_t, (N, 1)) * jnp.cos(phase_differences)).sum(1)
+        Isyn_0 = (A * amp_ratios * jnp.sin(phase_differences)).sum(1)
+
+        r = r.at[:, 0].set(
+            r_t
+            + dt * drdt(r_t, a, g, S, Isyn_r, Iext_r)
+            + eta * randn(size=(N,), seed=seed + t)
+        )
+
+        theta = theta.at[:, 0].set(
+            theta_t
+            + dt * d0dt(omegas, g, Isyn_0, Iext_0)
+            + eta * randn(size=(N,), seed=seed + t)
+        )
+
+        return (r, theta), r * jnp.exp(1j * theta)
+
+    _, z = jax.lax.scan(_loop, (r, theta), times)
+
+    return z[::decim].squeeze().T
 
 
 def simulate_delayed(
